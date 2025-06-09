8e4d861a411c92e8f67fa24f41850d91fbd11f57
kaique <kaique.souza.santos@escola.pr.gov.br>
2025-06-09 09:03:06 -0300

    .

--- atividades/ex 3.py
+++ atividades/ex 3.py
@@ -1,20 +1,11 @@
-# Constante para o valor de pi
 
 PI = 3.14159
 
-
-# Entrada do usuário
-
 raio = int(input("Digite o raio do círculo (inteiro): "))
-
-
-# Cálculos
 
 diametro = 2 * raio
 circunferencia = 2 * PI * raio
 area = PI * (raio **2)
-
-# Exibindo os resultados
 
 print("Diâmetro:", diametro)
 print("Circunferência:", circunferencia)
